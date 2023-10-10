575857e09ab5df0fdb450ad85dbfb69fc0609e94
nikaa <nikoloz.alaverdashvili.1@btu.edu.ge>
2023-10-10 11:25:39 +0400

    Demo Project

---
+++ src/main/auth/schema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class UserCreateSchema(BaseModel):
+    username: str
+    password: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "username": "nika2",
+                "password": "nika2"
+            }
+        }
